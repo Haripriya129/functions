4c60b1112a69cbc7f8a9eaa3d6aac9ae271b5db0
Haripriya129 <69579424+Haripriya129@users.noreply.github.com>
2021-06-04 14:16:55 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n;
+int i,r=1;
+int fact(int n)
+{
+    for(i=1;i<=n;i++)
+    {
+       r=r*i;
+
+    }
+    cout<<r;
+
+}
+int main()
+{
+    cin>>n;
+    fact(n);
+}
